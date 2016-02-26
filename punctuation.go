34f32661fd2b6baca1ce77f38798003bf187cffd
Matt Baer <matt@flashstuf.com>
2016-02-26 13:58:06 -0500

    Add comments

--- punctuation.go
+++ punctuation.go
@@ -7,8 +7,11 @@ import (
 	"strings"
 )
 
+// valid characters to extract
 const validPunc = ";:'\",!?.-()"
 
+// Extract scans the given File and returns only typical punctuation used in
+// prose.
 func Extract(f *os.File) string {
 	var out string
 	in := bufio.NewScanner(f)
